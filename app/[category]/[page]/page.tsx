2518fb77244193e7b84aeca9dbe504ebcd5b22db
Brock Penner <orbithammer@users.noreply.github.com>
2025-01-02 14:00:28 +0900

    Update page.tsx

--- app/[category]/[page]/page.tsx
+++ app/[category]/[page]/page.tsx
@@ -20,7 +20,7 @@ export default async function CategoryPage({ params }: PageParams) {
         <div>
             <p>Page: {page}</p>
             <div className='absolute top-[8%] left-2 sm:left-[10%] lg:top-[7.3%] flex'>
-              <h1 className="text-5xl font-bold z-10 text-gray-900 dark:text-white lg:text-6xl">
+              <h1 className="text-5xl font-bold z-10 text-gray-900 dark:text-white lg:text-6xl drop-shadow-[1px_1px_5px_rgba(255,255,255,0.5)] dark:drop-shadow-[1px_1px_5px_rgba(0,0,0,0.5)]">
                 isGlitch.com
               </h1>
               <Image 
