fee19ea94b1f5f22bb0c49f35b392c7c443f1bb1
Brock Penner <orbithammer@users.noreply.github.com>
2025-01-07 14:18:32 +0900

    removed notFound in CategoryPage

--- app/[category]/[page]/page.tsx
+++ app/[category]/[page]/page.tsx
@@ -1,4 +1,3 @@
-import { notFound } from 'next/navigation'
 import type { Metadata } from 'next'
 import { fetchArticles } from '@/lib/fetchArticles'
 import Pagination from '@/components/Pagination'
