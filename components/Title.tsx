fb065eb4337ba4e6d5bb80c994c8ebe21cf01e18
Brock Penner <orbithammer@users.noreply.github.com>
2025-01-08 07:13:31 +0900

    Update Title.tsx

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Title() {
     return (
-        <div className="fixed left-2 top-30 sm:left-[10%] z-10 flex items-center">
+        <div className="absolute left-2 top-30 sm:left-[10%] z-10 flex items-center">
             <h1 className="text-5xl font-bold z-15 text-gray-900 dark:text-white lg:text-6xl drop-shadow-[1px_1px_5px_rgba(255,255,255,0.5)] dark:drop-shadow-[1px_1px_5px_rgba(0,0,0,0.5)]">
                 isGlitch.com
             </h1>
