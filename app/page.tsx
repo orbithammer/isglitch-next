72b4165ceaf7fa91b539c2874f17d79ff17066ed
Brock Penner <orbithammer@users.noreply.github.com>
2025-01-15 16:02:50 +0900

    page.tsx redirects to page/[page]/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,5 @@
-import { fetchArticles } from '@/lib/fetchArticles'
-import Pagination from '@/components/Pagination'
-import ArticleList from '@/components/ArticleList'
-import Title from '@/components/Title'
+import { redirect } from 'next/navigation'
 
-export const dynamic = 'force-dynamic'
-
-export default async function HomePage() {
-    const page = 1
-    const category = "home"
-    const { articles, totalPages } = await fetchArticles(
-        category,
-        page
-    )
-
-    return (
-        <div>
-            <Pagination 
-              currentPage={page} 
-              totalPages={totalPages}
-              basePath="/page"
-            />
-            <Title />
-            <ArticleList articles={articles} />
-            <Pagination 
-              currentPage={page} 
-              totalPages={totalPages}
-              basePath="/page"
-            />
-        </div>
-    )
+export default function Home() {
+  redirect('/page/1')
 }
