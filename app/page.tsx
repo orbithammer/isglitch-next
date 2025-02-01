a9a17eb59982c46021fd72c2546178d0ce061eaa
Brock Penner <orbithammer@users.noreply.github.com>
2025-02-02 05:05:02 +0900

    added force dynamic to home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Pagination from '@/components/Pagination'
 import ArticleList from '@/components/ArticleList'
 import Title from '@/components/Title'
 
+export const dynamic = 'force-dynamic'
+
 export const metadata: Metadata = {
   title: `isGlitch.com - Where satire meets silicon`,
   description: `The online-est of tech rags. Where satire meets silicon and the truth isn't binary.`,
