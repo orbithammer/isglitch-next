55abf0fccbb029fe061846cf877ebaa53a01a0b4
Brock Penner <orbithammer@users.noreply.github.com>
2025-02-02 15:16:04 +0900

    HomePage basePath "/"

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,39 @@
-import { redirect } from 'next/navigation'
+import type { Metadata } from 'next'
+import { fetchArticles } from '@/lib/fetchArticles'
+import Pagination from '@/components/Pagination'
+import ArticleList from '@/components/ArticleList'
+import Title from '@/components/Title'
 
-export default function HomePage() {
-  redirect('/home/1')
+export const dynamic = 'force-dynamic'
+
+export const metadata: Metadata = {
+  title: `isGlitch.com - Where satire meets silicon`,
+  description: `The online-est of tech rags. Where satire meets silicon and the truth isn't binary.`,
+  openGraph: {
+    title: `isGlitch.com - Where satire meets silicon`,
+    description: `The online-est of tech rags. Where satire meets silicon and the truth isn't binary.`,
+    url: `https://isglitch.com`,
+    type: 'website'
+  }
+ }
+
+export default async function HomePage() {
+    const page = 1
+    const { articles, totalPages } = await fetchArticles(page, "home" );
+    return (
+        <div>
+            <Pagination 
+              currentPage={page} 
+              totalPages={totalPages}
+              basePath="/"
+            />
+            <Title />
+            <ArticleList articles={articles} />
+            <Pagination 
+              currentPage={page} 
+              totalPages={totalPages}
+              basePath="/"
+            />
+        </div>
+    )
 }
