9a22537511152b17c294494905a889a172c2edf1
Brock Penner <orbithammer@users.noreply.github.com>
2025-02-02 14:46:58 +0900

    route HomePage to /home/1

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,5 @@
-import type { Metadata } from 'next'
-import { fetchArticles } from '@/lib/fetchArticles'
-import Pagination from '@/components/Pagination'
-import ArticleList from '@/components/ArticleList'
-import Title from '@/components/Title'
+import { redirect } from 'next/navigation'
 
-export const dynamic = 'force-dynamic'
-
-export const metadata: Metadata = {
-  title: `isGlitch.com - Where satire meets silicon`,
-  description: `The online-est of tech rags. Where satire meets silicon and the truth isn't binary.`,
-  openGraph: {
-    title: `isGlitch.com - Where satire meets silicon`,
-    description: `The online-est of tech rags. Where satire meets silicon and the truth isn't binary.`,
-    url: `https://isglitch.com`,
-    type: 'website'
-  }
- }
-
-export default async function HomePage() {
-    const page = 1
-    const { articles, totalPages } = await fetchArticles(page, "home" );
-    return (
-        <div>
-            <Pagination 
-              currentPage={page} 
-              totalPages={totalPages}
-              basePath="/home"
-            />
-            <Title />
-            <ArticleList articles={articles} />
-            <Pagination 
-              currentPage={page} 
-              totalPages={totalPages}
-              basePath="/home"
-            />
-        </div>
-    )
+export default function HomePage() {
+  redirect('/home/1')
 }
