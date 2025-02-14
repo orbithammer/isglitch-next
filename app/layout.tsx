939e965515e1b621db4c3201a5e7b92b22f135e5
Brock Penner <orbithammer@users.noreply.github.com>
2025-02-14 19:19:59 +0900

    added script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import Script from 'next/script'
 import "./globals.css"
 import Header from "@/components/Header"
 import ThemeProvider from "@/lib/theme/ThemeProvider"
@@ -24,6 +25,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6716330636841627"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="sm:mx-[10%] mx-auto">
         <ThemeProvider>
           <Header />
