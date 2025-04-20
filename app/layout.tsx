f59d20409e39de1a435af95dc00621278ad47bf7
Brock Penner <orbithammer@users.noreply.github.com>
2025-04-20 10:54:09 +0900

    added metricool analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,22 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6716330636841627"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        <Script id="metricool-tracking" strategy="afterInteractive">
+            {`
+              function loadScript(a){
+                var b=document.getElementsByTagName("head")[0],
+                c=document.createElement("script");
+                c.type="text/javascript",
+                c.src="https://tracker.metricool.com/resources/be.js",
+                c.onreadystatechange=a,
+                c.onload=a,
+                b.appendChild(c)
+              }
+              loadScript(function(){
+                beTracker.t({hash:"46d3ba3cef2d0d3d6d9e7f956730041d"})
+              });
+            `}
+        </Script>
       </head>
       <body className="sm:mx-[10%] mx-auto">
         <ThemeProvider>
